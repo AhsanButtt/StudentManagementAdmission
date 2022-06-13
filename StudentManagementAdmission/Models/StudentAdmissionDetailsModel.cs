c7c054442ac0a6eb5cd476e870153258967ce218
AhsanButt <98800921+AhsanButt@users.noreply.github.com>
2022-06-13 23:10:27 +0500

    this is our first commit

---
+++ StudentManagementAdmission/Models/StudentAdmissionDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagementAdmission.Models
+{
+    public class StudentAdmissionDetailsModel
+    {
+
+
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string StudentClass { get; set; }
+        public DateTime DateOfJoining { get; set; }
+    }
+}
